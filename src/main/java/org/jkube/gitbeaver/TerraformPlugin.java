1b853dfae817abb5f537e702d9eac88e79c5a79c
Jan Kneissler <jan.kneissler@breuninger.de>
2022-12-07 15:38:31 +0100

    Move definition of secrets folder to core, export GOOGLE_APPLICATION_CREDENTIALS

--- src/main/java/org/jkube/gitbeaver/TerraformPlugin.java
+++ src/main/java/org/jkube/gitbeaver/TerraformPlugin.java
@@ -19,7 +19,8 @@ public class TerraformPlugin extends SimplePlugin {
                 "wget https://releases.hashicorp.com/terraform/0.12.21/terraform_0.12.21_linux_amd64.zip",
                 "unzip terraform_0.12.21_linux_amd64.zip",
                 "rm terraform_0.12.21_linux_amd64.zip",
-                "mv terraform /usr/bin/terraform"
+                "mv terraform /usr/bin/terraform",
+                "export GOOGLE_APPLICATION_CREDENTIALS="+GitBeaver.SECRETS_DIRECTORY+"terraform-service-account.json"
         );
     }
 
