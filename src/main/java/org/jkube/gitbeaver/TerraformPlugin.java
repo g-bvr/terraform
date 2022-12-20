7a5a26be91ad2c762e81e2b5b0ae68f326316a6d
Jan Kneissler <jan.kneissler@breuninger.de>
2022-12-20 21:30:32 +0100

    terraform version 1.3.6

--- src/main/java/org/jkube/gitbeaver/TerraformPlugin.java
+++ src/main/java/org/jkube/gitbeaver/TerraformPlugin.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class TerraformPlugin extends SimplePlugin {
 
+    private final static String TF_VERSION = "1.3.6";
     public TerraformPlugin() {
         super(
                 TerraformCommand.class
@@ -16,9 +17,9 @@ public class TerraformPlugin extends SimplePlugin {
     @Override
     public List<String> getInstallationScript() {
         return Arrays.asList(
-                "wget https://releases.hashicorp.com/terraform/0.12.21/terraform_0.12.21_linux_amd64.zip",
-                "unzip terraform_0.12.21_linux_amd64.zip",
-                "rm terraform_0.12.21_linux_amd64.zip",
+                "wget https://releases.hashicorp.com/terraform/"+TF_VERSION+"/terraform_"+TF_VERSION+"_linux_amd64.zip",
+                "unzip terraform_"+TF_VERSION+"_linux_amd64.zip",
+                "rm terraform_"+TF_VERSION+"_linux_amd64.zip",
                 "mv terraform /usr/bin/terraform"
         );
     }
