9b069a13aeacf61fb6dc7eb16926b9b71399493f
Jan Kneissler <jan.kneissler@breuninger.de>
2023-01-31 11:28:39 +0100

    Major refactoring of command parsing syntax, java 15 in core

--- src/main/java/org/jkube/gitbeaver/TerraformCommand.java
+++ src/main/java/org/jkube/gitbeaver/TerraformCommand.java
@@ -5,6 +5,8 @@ import org.jkube.gitbeaver.util.ExternalProcess;
 import java.util.List;
 import java.util.Map;
 
+import static org.jkube.gitbeaver.CommandParser.REST;
+
 
 public class TerraformCommand extends AbstractCommand {
     /**
@@ -14,13 +16,15 @@ public class TerraformCommand extends AbstractCommand {
     private static final String TERRAFORM_BINARY = "/usr/bin/terraform";
 
     public TerraformCommand() {
-        super(1, null, "terraform");
+        super("Execute terraform command");
+        commandline("TERRAFORM *");
+        argument(REST, "the command to be executed (the commandline arguments to be passed to the terraform executable)");
     }
 
     @Override
-    public void execute(Map<String, String> variables, WorkSpace workSpace, List<String> arguments) {
+    public void execute(Map<String, String> variables, WorkSpace workSpace, Map<String, String> arguments) {
         ExternalProcess terraform = new ExternalProcess();
-        terraform.command(TERRAFORM_BINARY, arguments);
+        terraform.command(TERRAFORM_BINARY, List.of(arguments.get(REST).split(" ")));
         terraform
                 .dir(workSpace.getWorkdir())
                 .successMarker("Created ")
